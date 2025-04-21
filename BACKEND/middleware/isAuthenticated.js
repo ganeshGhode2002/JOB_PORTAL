de11047c110ef4cb10cc7679fb2042a8cf4ae6e2
Ganesh Ghode <ganeshghode2002@gmail.com>
2025-04-21 15:32:16 +0530

    middleware

---
+++ BACKEND/middleware/isAuthenticated.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken'
+
+const isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookie.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "token not found",
+                succes: false
+            })
+        }
+        const decode = await jwt.verify(token, process.env.SECRETE_KEY)
+        if (!decode) {
+            return res.status(401).json({
+                message: "invalid token",
+                succes: false
+            })
+        }
+
+        req.id = decode.userId;
+        next();
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default isAuthenticated;
